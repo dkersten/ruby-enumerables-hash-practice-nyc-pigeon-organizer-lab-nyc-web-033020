629020d2559833865b13aa373694de1e1da938b2
Daniel Kersten <dkersten1994@gmail.com>
2020-03-17 14:20:41 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  
+  # loop through the colors and get the names. Then add those names as a key in a new hash
+  
+  # find name in gender. If male/female add key value pair in new hash
+  
+  # find name in lives. Find where they live and add as key value pair in new hash
+  
+  i = 0
+  formatted_pigeon_data = {}
+  
+  while i < data
 end
